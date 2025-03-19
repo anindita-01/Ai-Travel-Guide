87b57c0fd97df1836242dbd6458c282b5de6b7e1
Anindita Dey <aninditadey@Aninditas-MacBook-Air.local>
2025-03-19 12:40:53 +0530

    initial setup

---
+++ ai-travel-guide/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      hiiii
+      <Button>die</Button>
+    </>
+  );
+}
+
+export default App;
